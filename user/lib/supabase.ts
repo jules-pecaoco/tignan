d4eadeb854099fc19ce462662739da32efe5e57c
jules-pecaoco <jules.pecaoco.dev@gmail.com>
2026-04-06 10:42:23 +0800

    Revert "initialize evacuee or user"
    This reverts commit f02a8d1c3e9ef54a6b014a23e8d87d8f34266346.

--- user/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// For Expo, we use process.env to access environment variables starting with EXPO_PUBLIC_
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
